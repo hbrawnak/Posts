74488a95341d28974dab57d97ba3f8fdfbb0a113
Habib <hbrawnak@gmail.com>
2018-09-08 22:34:16 +0600

    Layout added and a few design

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Habib',
+        'title': 'It\'s a title',
+        'content': 'This is first content',
+        'created_at': 'September 4'
+    },
+    {
+        'author': 'Jui',
+        'title': 'It\'s a title 2',
+        'content': 'This is second content',
+        'created_at': 'September 5'
+    }
+]
+
+
 def home(request):
-    context = {'title': 'Home' }
+    context = {
+        'title': 'Home',
+        'posts': posts
+    }
     return render(request, 'blog/home.html', context)
 
 def about(request):
