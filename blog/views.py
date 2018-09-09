814ca1ca74dabe6c2633bd42b81861b758aecd89
Habib <hbrawnak@gmail.com>
2018-09-09 23:46:45 +0600

    Migration created and Post from database

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Habib',
-        'title': 'It\'s a title',
-        'content': 'This is first content',
-        'created_at': 'September 4'
-    },
-    {
-        'author': 'Jui',
-        'title': 'It\'s a title 2',
-        'content': 'This is second content',
-        'created_at': 'September 5'
-    }
-]
 
 
 def home(request):
+
+    posts = Post.objects.all()
+
     context = {
         'title': 'Home',
         'posts': posts
