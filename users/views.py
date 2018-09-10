36e9c0c7ab20ab9f406ac3d270b5c3b323d81336
Habib <hbrawnak@gmail.com>
2018-09-11 00:00:56 +0600

    Layout fixed

--- users/views.py
+++ users/views.py
@@ -12,4 +12,4 @@ def register(request):
             return redirect('blog-home')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form, 'title': 'Registration'})
