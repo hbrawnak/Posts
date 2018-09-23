5a18c84c6b925f53698c0da4e4e4e82c3e0e01f6
Habib <hbrawnak@gmail.com>
2018-09-23 23:13:43 +0600

    Profile update is working

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdate
 
 def register(request):
     if request.method == 'POST':
@@ -17,5 +17,25 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == "POST":
+        userUpdateForm = UserUpdateForm(request.POST, instance=request.user)
+        profileUpdateForm = ProfileUpdate(request.POST, request.FILES, instance=request.user.profile)
+
+        if userUpdateForm.is_valid() and profileUpdateForm.is_valid():
+            userUpdateForm.save()
+            profileUpdateForm.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+
+    else:
+        userUpdateForm = UserUpdateForm(instance=request.user)
+        profileUpdateForm = ProfileUpdate(instance=request.user.profile)
+
+    context = {
+        'userUpdateForm': userUpdateForm,
+        'profileUpdateForm': profileUpdateForm,
+        'title': 'Profile Update'
+    }
+
+    return render(request, 'users/profile.html', context)
 
